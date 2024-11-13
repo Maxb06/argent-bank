1b93bbd54b3684c6f105fa699152806e87ca543e
Maxime Brunet <maximb84@gmail.com>
2024-11-13 14:22:32 +0100

    style : Layout in src with header & footer

---
+++ src/layout/Header/Header.jsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../store/authSlice';
+import styles from './header.module.scss';
+import logo from '../../assets/img/argentBankLogo.png';
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+  const user = useSelector((state) => state.auth.user);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <img src={logo} alt="Logo Argent Bank" className={styles.header__logo} />
+      </Link>
+      <nav className={styles.header__nav}>
+        {token && user ? (
+          <>
+            <Link to="/user" className={styles.header__nav__item}>
+              <i className="fa fa-user-circle"></i>
+              {user.firstName}
+            </Link>
+            <button onClick={handleLogout} className={styles.header__nav__item}>
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </button>
+          </>
+        ) : (
+          <Link to="/login" className={styles.header__nav__item}>
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
